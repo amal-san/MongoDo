59953ed45b5f9ce500727b3ff838bca3549c7ab6
amal-san <amaldemaniax@gmail.com>
2020-02-19 20:26:28 +0530

    port number

---
+++ server/src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 4000
+const graphqlHTTP = require('express-graphql');
+
+
+const schema = {
+  // we will add this later
+};
+
+app.use(cors());
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+    pretty: true
+  })
+);
+
+app.listen(port);
+console.log('Server is listening in %s',port);

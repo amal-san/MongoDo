f706b97751fc5c5789d70f51505194857f69255c
amal-san <amaldemaniax@gmail.com>
2020-02-20 13:38:26 +0530

    schema add

--- server/src/index.js
+++ server/src/index.js
@@ -1,25 +1,21 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./graphql/schema');
+const { setupDB } = require('./config/databaseConnection');
 const cors = require('cors');
 const app = express();
 const port = 4000
-const graphqlHTTP = require('express-graphql');
-const { setupDB } = require('./config/databaseConnection')
 
 setupDB(v => console.log(v));
 
-
-const schema = {
-  // we will add this later
-};
-
 app.use(cors());
 app.use(
-  '/graphql',
-  graphqlHTTP({
-    schema,
-    graphiql: true,
-    pretty: true
-  })
+    '/graphql',
+    graphqlHTTP({
+        schema,
+        graphiql: true,
+        pretty: true
+    })
 );
 
 app.listen(port);
